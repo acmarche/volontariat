e1c1e005e8d7b6cc294798a02ddc308cd4a8ae2e
jfsenechal <jfsenechal@gmail.com>
2023-03-23 17:08:50 +0100

    fix

--- src/Controller/Admin/PageController.php
+++ src/Controller/Admin/PageController.php
@@ -7,8 +7,6 @@ use AcMarche\Volontariat\Form\Admin\PageType;
 use AcMarche\Volontariat\Repository\PageRepository;
 use AcMarche\Volontariat\Service\FileHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -64,7 +62,6 @@ class PageController extends AbstractController
     #[Route(path: '/{id}', name: 'volontariat_admin_page_show', methods: ['GET'])]
     public function showAction(Page $page): Response
     {
-        $deleteForm = $this->createDeleteForm($page);
         $images = $this->fileHelper->getImages($page);
         $docs = $this->fileHelper->getDocuments($page);
 
@@ -74,7 +71,6 @@ class PageController extends AbstractController
                 'page' => $page,
                 'images' => $images,
                 'documents' => $docs,
-                'delete_form' => $deleteForm->createView(),
             ]
         );
     }
@@ -82,10 +78,10 @@ class PageController extends AbstractController
     #[Route(path: '/{id}/edit', name: 'volontariat_admin_page_edit', methods: ['GET', 'POST'])]
     public function editAction(Request $request, Page $page): Response
     {
-        $editForm = $this->createForm(PageType::class, $page);
+        $form = $this->createForm(PageType::class, $page);
 
-        $editForm->handleRequest($request);
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
             $this->pageRepository->flush();
             $this->addFlash('success', 'La page a bien été modifiée');
 
@@ -96,31 +92,20 @@ class PageController extends AbstractController
             '@Volontariat/admin/page/edit.html.twig',
             [
                 'page' => $page,
-                'edit_form' => $editForm->createView(),
+                'form' => $form->createView(),
             ]
         );
     }
 
-    #[Route(path: '/{id}', name: 'volontariat_admin_page_delete', methods: ['DELETE'])]
-    public function deleteAction(Request $request, Page $page): RedirectResponse
+    #[Route(path: '/{id}/delete', name: 'volontariat_admin_page_delete', methods: ['POST'])]
+    public function delete(Request $request, Page $page): RedirectResponse
     {
-        $form = $this->createDeleteForm($page);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($this->isCsrfTokenValid('delete'.$page->getId(), $request->request->get('_token'))) {
             $this->pageRepository->remove($page);
             $this->pageRepository->flush();
             $this->addFlash('success', 'La page a bien été supprimée');
         }
 
         return $this->redirectToRoute('volontariat_admin_page');
-    }
-
-    private function createDeleteForm(Page $page): FormInterface
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('volontariat_admin_page_delete', ['id' => $page->getId()]))
-            ->setMethod(Request::METHOD_DELETE)
-            ->add('submit', SubmitType::class, ['label' => 'Delete', 'attr' => ['class' => 'btn-danger']])
-            ->getForm();
     }
 }

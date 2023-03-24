a96a30aa420628a238b377d6526c9e01a1904524
jfsenechal <jfsenechal@gmail.com>
2023-03-24 13:07:18 +0100

    add secteurs

--- src/Controller/SecteurController.php
+++ src/Controller/SecteurController.php
@@ -2,37 +2,39 @@
 
 namespace AcMarche\Volontariat\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Response;
 use AcMarche\Volontariat\Entity\Secteur;
+use AcMarche\Volontariat\Repository\SecteurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(path: '/secteur')]
 class SecteurController extends AbstractController
 {
-    public function __construct(private ManagerRegistry $managerRegistry)
+    public function __construct(private SecteurRepository $secteurRepository)
     {
     }
+
     #[Route(path: '/', name: 'volontariat_secteur')]
-    public function indexAction() : Response
+    public function indexAction(): Response
     {
-        $em = $this->managerRegistry->getManager();
-        $secteurs = $em->getRepository(Secteur::class)->findAll();
+        $secteurs = $this->secteurRepository->findAll();
+
         return $this->render('@Volontariat/secteur/index.html.twig', [
             'secteurs' => $secteurs,
         ]);
     }
 
     #[Route(path: '/{id}', name: 'volontariat_secteur_show', methods: ['GET'])]
-    public function showAction(Secteur $secteur) : Response
+    public function showAction(Secteur $secteur): Response
     {
         $associations = $secteur->getAssociations();
         $volontaires = $secteur->getVolontaires();
-        return $this->render('@Volontariat/secteur/index.html.twig', array(
+
+        return $this->render('@Volontariat/secteur/show.html.twig', [
             'secteur' => $secteur,
             'associations' => $associations,
             'volontaires' => $volontaires,
-        ));
+        ]);
     }
 }

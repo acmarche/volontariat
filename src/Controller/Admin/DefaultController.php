8f8758cf89a5769a7639226053b7835659574745
jfsenechal <jfsenechal@gmail.com>
2023-04-25 09:38:45 +0200

    add link admin

--- src/Controller/Admin/DefaultController.php
+++ src/Controller/Admin/DefaultController.php
@@ -2,17 +2,17 @@
 
 namespace AcMarche\Volontariat\Controller\Admin;
 
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(path: '/admin')]
 #[IsGranted('ROLE_VOLONTARIAT_ADMIN')]
 class DefaultController extends AbstractController
 {
     #[Route(path: '/', name: 'volontariat_admin_home')]
-    public function index() : Response
+    public function index(): Response
     {
         return $this->render('@Volontariat/admin/default/index.html.twig');
     }

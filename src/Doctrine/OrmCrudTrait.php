9f7b3ba732857ca3fdd919411ddfedaa091598b5
jfsenechal <jfsenechal@gmail.com>
2024-02-06 08:36:16 +0100

    up

--- src/Doctrine/OrmCrudTrait.php
+++ src/Doctrine/OrmCrudTrait.php
@@ -6,16 +6,6 @@ use Doctrine\ORM\EntityManager;
 
 trait OrmCrudTrait
 {
-    /**
-     * @var EntityManager
-     */
-    protected $_em;
-
-    public function getEntityManger(): EntityManager
-    {
-        return $this->_em;
-    }
-
     public function insert(object $object): void
     {
         $this->persist($object);
@@ -24,21 +14,22 @@ trait OrmCrudTrait
 
     public function persist(object $object): void
     {
-        $this->_em->persist($object);
+        $this->getEntityManager()->persist($object);
     }
 
     public function flush(): void
     {
-        $this->_em->flush();
+        $this->getEntityManager()->flush();
     }
 
     public function remove(object $object): void
     {
-        $this->_em->remove($object);
+        $this->getEntityManager()->remove($object);
     }
 
     public function getOriginalEntityData(object $object)
     {
-        return $this->_em->getUnitOfWork()->getOriginalEntityData($object);
+        return $this->getEntityManager()->getUnitOfWork()->getOriginalEntityData($object);
     }
+
 }

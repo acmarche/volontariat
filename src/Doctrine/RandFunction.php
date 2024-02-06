bc3745a91655b36553469e9aee6c3fc5184cfc59
jfsenechal <jfsenechal@gmail.com>
2024-02-06 13:46:47 +0100

    fix type

--- src/Doctrine/RandFunction.php
+++ src/Doctrine/RandFunction.php
@@ -11,7 +11,8 @@ namespace AcMarche\Volontariat\Doctrine;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
+
 
 /**
  * RandFunction ::= "RAND" "(" ")"
@@ -20,9 +21,9 @@ class RandFunction extends FunctionNode
 {
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker): string

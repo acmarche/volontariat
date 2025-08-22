7a0f3cf303323efdef6c7b1c2d74b2ea3f0c1a99
jfsenechal <jfsenechal@gmail.com>
2025-08-22 11:46:13 +0200

    fix

--- src/Spam/Handler/SpamHandler.php
+++ src/Spam/Handler/SpamHandler.php
@@ -4,7 +4,7 @@ namespace AcMarche\Volontariat\Spam\Handler;
 
 use AcMarche\Volontariat\Spam\CaptchaApi;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\RateLimiter\RateLimiterFactory;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
@@ -13,14 +13,14 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class SpamHandler
 {
     public function __construct(
-        private readonly RateLimiterFactory $rateLimiterFactory,
+        private readonly RateLimiterFactoryInterface $anonymousApiLimiter,
         private readonly CaptchaApi $captchaApi
     ) {
     }
 
     public function isAccepted(Request $request): bool
     {
-        $limiter = $this->rateLimiterFactory->create($request->getClientIp());
+        $limiter = $this->anonymousApiLimiter->create($request->getClientIp());
 
         return $limiter->consume()->isAccepted();
     }

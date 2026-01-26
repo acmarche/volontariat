7450ce5b656aeeb8017c8845151ab626faded9ed
Senechal jf <jfsenechal@gmail.com>
2026-01-26 15:15:59 +0100

    add help text for email field in AssocationType form

--- src/Form/Admin/AssocationType.php
+++ src/Form/Admin/AssocationType.php
@@ -38,6 +38,7 @@ class AssocationType extends AbstractType
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Courriel',
+                'help' => 'zeez',
             ])
             ->add(
                 'web_site',
